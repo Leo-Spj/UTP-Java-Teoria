c0682f4c1cc4f8b2e7d325f6193f5aa01d5f0d39
LeoSpj <leonardocesarespejo@gmail.com>
2022-08-23 18:14:34 -0500

    estructurando archivos

---
+++ src/Semana1/S1_tarea_ejercicio1.java
@@ -0,0 +1,21 @@
+package Semana1;
+
+import java.util.Scanner;
+
+public class S1_tarea_ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese el precio del producto: ");
+        double montoMes = teclado.nextDouble();
+
+        System.out.println("Ingrese el número de cuotas: ");
+        double nroMeses = teclado.nextDouble();
+
+        double cuota = montoMes / nroMeses;
+        double redondeo = (double)Math.round(cuota * 100.0) / 100.0;
+
+        System.out.println("El monto a pagar es: " + redondeo);
+    }
+}
---
+++ src/Semana2/S2_teoria_ejercicio2.java
@@ -0,0 +1,6 @@
+package Semana2;
+
+public class S2_teoria_ejercicio2 {
+
+
+}
