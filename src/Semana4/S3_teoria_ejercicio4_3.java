173bdf008192b2629e942d48ab1dc8292ca8364c
LeoSpj <leonardocesarespejo@gmail.com>
2022-09-06 13:00:45 -0500

    semana 4 sesión 1

---
+++ src/Semana4/S3_teoria_ejercicio4_3.java
@@ -0,0 +1,60 @@
+package Semana4;
+/*un cliente desea comprar una cantidad de pantalones del mismo tipo, se ofrecen los seguientes precios unitarios segun el tipo:
+tipo
+deportivo - 50
+casual - 40
+elegante - 70
+
+descuento
+1-10  3%
+11=16  5%
+17 en adelante  7%
+ */
+
+import java.util.Scanner;
+
+public class S3_teoria_ejercicio4_3 {
+    public static void main(String[] args) {
+        String tipo="", msg="El precio es de S/";
+        int precio, cantidad;
+        double descuento;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese el tipo de pantalon: ");
+        tipo = teclado.next();
+        tipo = tipo.toLowerCase();
+
+        System.out.println("Ingrese la cantidad de pantalones: ");
+        cantidad = teclado.nextInt();
+
+        switch (tipo){
+            case "deportivo":
+                precio = 50;
+                break;
+            case "casual":
+                precio = 40;
+                break;
+            case "elegante":
+                precio = 70;
+                break;
+            default:
+                precio = 0;
+                msg = "Tipo de pantalon no valido ";
+                break;
+        }
+
+        if (cantidad >= 1 && cantidad <= 10){
+            descuento = 0.97;
+        } else if (cantidad > 10 && cantidad <= 16){
+            descuento = 0.95;
+        } else if (cantidad > 16){
+            descuento = 0.93;
+        } else {
+            descuento = 1;
+        }
+
+        System.out.println(msg + Math.round((cantidad*(precio*descuento))*100)/100.0 );
+    }
+}
+
---
+++ src/Semana4/S4_teoria_switch.java
@@ -0,0 +1,40 @@
+package Semana4;
+
+import javafx.scene.control.TableColumn;
+
+import java.util.Scanner;
+
+/*
+* El  banco  “Pueblo  desconocido”  ha  decidido  aumentar  el  límite  de  crédito  de  las  tarjetas  de
+crédito  de  sus  clientes,  para  esto  considera  que,  si  su  cliente  tiene  tarjeta  tipo  1,  el  aumento
+será de 25 %; si tiene tipo 2, será de 35 %; si tiene tipo 3, de 40 %, y para cualquier otro tipo,
+de  10  %.  Si  la  persona  tiene  deuda  con  el  banco  solo  se  le  dará  la  mitad  de  incremento,  si  es
+que  es  cliente  1  o  2,  el  resto  no  tiene  incremento.  Realice  un  algoritmo  para  determinar  el
+nuevo límite de crédito que tendrá una persona en su tarjeta.
+* */
+
+public class S4_teoria_switch {
+
+   public static void main(String[] args) {
+      int tipo; double porctInc; String deuda;
+      Scanner scan=new Scanner(System.in);
+      System.out.print("Tipo "); tipo=scan.nextInt();
+      System.out.print("Tiene deuda? (SI/NO "); deuda=scan.next();
+      deuda=deuda.toUpperCase();
+      switch(tipo){
+         case 1: porctInc=0.25;break;
+         case 2: porctInc=0.35;break;
+         case 3: porctInc=0.40;break;
+         default: porctInc=0.25;
+      }
+      if(deuda.equals("SI")){
+         switch(tipo){
+            case 1:
+            case 2: porctInc=porctInc/2;break;
+            default:porctInc=0;
+         }
+      }
+      System.out.println("Porct Incremento "+porctInc);
+   }
+
+}
